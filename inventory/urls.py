4df1d1d7fc740f8178835517fbbfe8d0c72491a6
sajib1066 <sajib1066@gmail.com>
2020-07-20 10:27:02 +0600

    setup dashboard template

--- inventory/urls.py
+++ inventory/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import HomePageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
 ]
