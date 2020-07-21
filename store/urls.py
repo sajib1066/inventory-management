1938cb24bed8b8a236f249389d8de1218eccaa94
sajib1066 <sajib1066@gmail.com>
2020-07-21 15:20:33 +0600

    create buyer done

--- store/urls.py
+++ store/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
     create_supplier,
     SupplierListView,
+    create_buyer
 )
 
 urlpatterns = [
     path('create-supplier/', create_supplier, name='create-supplier'),
     path('supplier-list/', SupplierListView.as_view(), name='supplier-list'),
+    path('create-buyer/', create_buyer, name='create-buyer'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,10 +3,12 @@ from django.views.generic import ListView
 
 from users.models import User
 from .models import (
-    Supplier
+    Supplier,
+    Buyer
 )
-from .forms import SupplierForm
+from .forms import SupplierForm, BuyerForm
 
+# Supplier views
 def create_supplier(request):
     forms = SupplierForm()
     if request.method == 'POST':
@@ -32,3 +34,25 @@ class SupplierListView(ListView):
     model = Supplier
     template_name = 'store/supplier_list.html'
     context_object_name = 'supplier'
+
+
+# Buyer views
+def create_buyer(request):
+    forms = BuyerForm()
+    if request.method == 'POST':
+        forms = BuyerForm(request.POST)
+        if forms.is_valid():
+            name = forms.cleaned_data['name']
+            address = forms.cleaned_data['address']
+            email = forms.cleaned_data['email']
+            username = forms.cleaned_data['username']
+            password = forms.cleaned_data['password']
+            retype_password = forms.cleaned_data['retype_password']
+            if password == retype_password:
+                user = User.objects.create_user(username=username, password=password, email=email, is_buyer=True)
+                Buyer.objects.create(user=user, name=name, address=address)
+                return redirect('home')
+    context = {
+        'form': forms
+    }
+    return render(request, 'store/create_buyer.html', context)
