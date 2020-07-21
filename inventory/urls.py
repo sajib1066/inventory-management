ca017bf3e77539aef82cbd45b0acae5334b93ee0
sajib1066 <sajib1066@gmail.com>
2020-07-21 17:01:00 +0600

    update dashboard home page

--- inventory/urls.py
+++ inventory/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import HomePageView
+from .views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomePageView.as_view(), name='home'),
+    path('', dashboard, name='dashboard'),
     path('store/', include('store.urls')),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
 
+from store.models import Product, Supplier, Buyer
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+def dashboard(request):
+    total_product = Product.objects.count()
+    total_supplier = Supplier.objects.count()
+    total_buyer = Buyer.objects.count()
+    context = {
+        'product': total_product,
+        'supplier': total_supplier,
+        'buyer': total_buyer
+    }
+    return render(request, 'dashboard.html', context)
