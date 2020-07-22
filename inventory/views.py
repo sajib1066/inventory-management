9378a8588c1630e23b105bf087e97dc2f1cc4b53
sajib1066 <sajib1066@gmail.com>
2020-07-22 10:51:45 +0600

    add order list in dashboard home page

--- inventory/views.py
+++ inventory/views.py
@@ -7,10 +7,12 @@ def dashboard(request):
     total_supplier = Supplier.objects.count()
     total_buyer = Buyer.objects.count()
     total_oder = Order.objects.count()
+    orders = Order.objects.all()
     context = {
         'product': total_product,
         'supplier': total_supplier,
         'buyer': total_buyer,
-        'order': total_oder
+        'order': total_oder,
+        'orders': orders
     }
     return render(request, 'dashboard.html', context)
