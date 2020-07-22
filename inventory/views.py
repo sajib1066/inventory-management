9155a8f0527622c8f72826d03ac8b793ec51fda1
sajib1066 <sajib1066@gmail.com>
2020-07-22 10:04:10 +0600

    update order counts

--- inventory/views.py
+++ inventory/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
-from store.models import Product, Supplier, Buyer
+from store.models import Product, Supplier, Buyer, Order
 
 def dashboard(request):
     total_product = Product.objects.count()
     total_supplier = Supplier.objects.count()
     total_buyer = Buyer.objects.count()
+    total_oder = Order.objects.count()
     context = {
         'product': total_product,
         'supplier': total_supplier,
-        'buyer': total_buyer
+        'buyer': total_buyer,
+        'order': total_oder
     }
     return render(request, 'dashboard.html', context)
